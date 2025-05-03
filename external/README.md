815a36e342d4ab4829cb24916070d63d240cb2cf
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-05-03 10:56:04 +0200

    .

--- external/README.md
+++ external/README.md
@@ -5,6 +5,3 @@
 - [nob.h](https://github.com/tsoding/nob.h)
 
 ## Libarys
-
-- [raylib](https://github.com/raysan5/raylib)
-- [raygui](https://github.com/raysan5/raygui)
--- nob.c
+++ nob.c
@@ -20,41 +20,20 @@ bool init_env()
   return mkdir_if_not_exists(BUILD_DIR);
 }
 
-bool build_raylib(Cmd *cmd) {
-    //if (!mkdir_if_not_exists(BUILD_DIR "/raylib")) return 1;
-    // Build raylib
-    cmd_append(cmd, "make",
-               temp_sprintf("RAYLIB_RELEASE_PATH=%s/raylib", BUILD_DIR),
-               "PLATFORM=PLATFORM_DESKTOP",
-               temp_sprintf("RAYLIB_MODULE_RAYGUI_PATH=%s/lib/raygui/src", EXTERNAL_DIR),
-               "GLFW_LINUX_ENABLE_WAYLAND=TRUE",
-               "USE_EXTERNAL_GLFW=TRUE",
-               "RAYLIB_MODULE_RAYGUI=TRUE",
-               "-C", temp_sprintf("%s/lib/raylib/src", EXTERNAL_DIR));
-    return cmd_run_sync_and_reset(cmd);
-}
-
 bool build_Visonic(Cmd *cmd) {
     // Build Visonic
     cmd_append(cmd,
                "g++",
-               "-o",
-               temp_sprintf("%s/visonic", BUILD_DIR),
-               "-Wall", "-Wextra", "-Wwritable-strings", temp_sprintf("%s/main.cpp", SRC_DIR),
-               temp_sprintf("-I%s/lib/raylib/src", EXTERNAL_DIR),
-               temp_sprintf("-L%s/raylib", BUILD_DIR),
-               "-lraylib", "-lm", "-ldl", "-lpthread");
-#ifdef __APPLE__
-      cmd_append(cmd, "-framework", "Cocoa", "-framework", "IOKit", "-framework", "CoreFoundation", "-framework", "CoreGraphics");
-    #endif
+               "-o", temp_sprintf("%s/visonic", BUILD_DIR),
+               "-Wall", "-Wextra", "-Wwritable-strings",
+               temp_sprintf("%s/main.cpp", SRC_DIR));
     return cmd_run_sync_and_reset(cmd);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
     if (!init_env()) return 1;
-    if (!build_raylib(&cmd)) return 1;
     if (!build_Visonic(&cmd)) return 1;
 
   return 0;
